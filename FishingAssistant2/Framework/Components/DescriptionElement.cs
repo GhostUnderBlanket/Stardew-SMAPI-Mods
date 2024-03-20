a21ebc5207136b61b1ceae48712b38c7bc6b6559
ChibiKyu <ninepenguin.live@gmail.com>
2024-03-20 14:33:44 +0700

    Add menu option elements

---
+++ FishingAssistant2/Framework/Components/DescriptionElement.cs
@@ -0,0 +1,33 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewValley;
+using StardewValley.Menus;
+
+namespace FishingAssistant2.Framework.Components
+{
+    /// This code is edited version of CJBok and Pathoschild DescriptionElement in CJBCheatsMenu repository
+    /// https://github.com/CJBok/SDV-Mods/blob/master/CJBCheatsMenu/Framework/Components/DescriptionElement.cs
+    /// available under the MIT License. See that repository for the latest version.
+    
+    /// <summary>An options element which contains descriptive text.</summary>
+    internal class DescriptionElement : BaseOptionsElement
+    {
+        /*********
+         ** Public methods
+         *********/
+        /// <summary>Construct an instance.</summary>
+        /// <param name="label">The checkbox label.</param>
+        public DescriptionElement(string label)
+            : base(label, -1, -1, 0, 0, 0) { }
+
+        /// <summary>Draw the component to the screen.</summary>
+        /// <param name="spriteBatch">The sprite batch being drawn.</param>
+        /// <param name="slotX">The X position at which to draw, relative to the bounds.</param>
+        /// <param name="slotY">The Y position at which to draw, relative to the bounds.</param>
+        /// <param name="context">The menu drawing the component.</param>
+        public override void draw(SpriteBatch spriteBatch, int slotX, int slotY, IClickableMenu? context = null)
+        {
+            spriteBatch.DrawString(Game1.smallFont, this.label, new Vector2(slotX + this.bounds.X, slotY + this.bounds.Y), Color.Black);
+        }
+    }
+}
