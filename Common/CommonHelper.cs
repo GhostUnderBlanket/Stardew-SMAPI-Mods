d8cb01788a03837d58d1c5f5b25fb9c601d9708d
ChibiKyu <ninepenguin.live@gmail.com>
2024-04-25 01:32:37 +0700

    Update CommonHelper.cs

--- Common/CommonHelper.cs
+++ Common/CommonHelper.cs
@@ -6,27 +6,43 @@ namespace ChibiKyu.StardewMods.Common
 {
     public static class CommonHelper
     {
-        internal static void PushNotification(int whatType, string key, params object[] args)
+        private static void PushNotification(int whatType, Item? item, string key, params object[] args)
         {
             if (!Context.IsWorldReady) return;
 
-            Game1.addHUDMessage(new HUDMessage(string.Format(key, args), whatType));
+            HUDMessage hudMessage = new(string.Format(key, args), whatType) { messageSubject = item, timeLeft = 3300 };
+            Game1.addHUDMessage(hudMessage);
         }
 
-        internal static void PushWarnNotification(string key, params object[] args)
+        private static void PushNotification(int whatType, string key, params object[] args)
+        {
+            PushNotification(whatType, null, key, args);
+        }
+
+        internal static void PushWarning(string key, params object[] args)
         {
             PushNotification(HUDMessage.newQuest_type, key, args);
         }
 
-        internal static void PushErrorNotification(string key, params object[] args)
+        internal static void PushWarning(Item item, string key, params object[] args)
+        {
+            PushNotification(HUDMessage.newQuest_type, item, key, args);
+        }
+
+        internal static void PushError(string key, params object[] args)
         {
             PushNotification(HUDMessage.error_type, key, args);
         }
 
-        internal static void PushToggleNotification(bool toggleValue, string message)
+        internal static void PushError(Item item, string key, params object[] args)
+        {
+            PushNotification(HUDMessage.newQuest_type, item, key, args);
+        }
+
+        internal static void PushToggle(bool toggleValue, string message)
         {
             string status = toggleValue ? I18n.On() : I18n.Off();
-            PushWarnNotification(message, status);
+            PushWarning(message, status);
         }
     }
 }
