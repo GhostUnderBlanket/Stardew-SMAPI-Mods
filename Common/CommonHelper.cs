58c77058c056f4accd77baeaf126b8c31283a1bd
ChibiKyu <ninepenguin.live@gmail.com>
2024-04-27 00:50:17 +0700

    Update CommonHelper.cs

--- Common/CommonHelper.cs
+++ Common/CommonHelper.cs
@@ -1,11 +1,17 @@
 using FishingAssistant2;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
 using StardewValley;
+using StardewValley.Menus;
+using Object = StardewValley.Object;
 
 namespace ChibiKyu.StardewMods.Common
 {
     public static class CommonHelper
     {
+        private static readonly Rectangle TextureBoxSource = new(0, 256, 60, 60);
+
         private static void PushNotification(int whatType, Item? item, string key, params object[] args)
         {
             if (!Context.IsWorldReady) return;
@@ -43,6 +49,38 @@ namespace ChibiKyu.StardewMods.Common
         {
             string status = toggleValue ? I18n.On() : I18n.Off();
             PushWarning(message, status);
+        }
+
+        internal static void DrawString(string? text, Vector2 drawPos, float scale)
+        {
+            Game1.spriteBatch.DrawString(Game1.dialogueFont, text, drawPos, Color.Black, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+        }
+
+        internal static void DrawSprite(Object? obj, Vector2 drawPos, float scale)
+        {
+            DrawSprite(obj, drawPos, scale, Color.White);
+        }
+
+        internal static void DrawSprite(Object? obj, Vector2 drawPos, float scale, Color color)
+        {
+            obj?.drawInMenu(Game1.spriteBatch, drawPos, scale, 1.0f, 1.0f, StackDrawType.Hide, color, false);
+        }
+
+        internal static void DrawToggleIcon(bool value, Rectangle source, int x, int y, int size = 20, float scale = 1.0f, float trueTrans = 1.0f, float falseTrans = 0.2f)
+        {
+            ClickableTextureComponent icon = new(new Rectangle(x, y, size, size), Game1.mouseCursors, source, scale);
+            float transparency = value ? trueTrans : falseTrans;
+            icon.draw(Game1.spriteBatch, Color.White * transparency, 0);
+        }
+
+        internal static void DrawTextureBox(int x, int y, int size, Color color, bool drawShadow = false)
+        {
+            DrawTextureBox(x, y, size, size, color, drawShadow: drawShadow);
+        }
+
+        internal static void DrawTextureBox(int x, int y, int width, int height, Color color, float scale = 1f, bool drawShadow = false)
+        {
+            IClickableMenu.drawTextureBox(Game1.spriteBatch, Game1.menuTexture, TextureBoxSource, x, y, width, height, color, scale, drawShadow);
         }
     }
 }
